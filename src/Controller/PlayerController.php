2dd61bf877589b324d12d753d6a77b8c3cbc9046
Alejandro Hernandez <alejandrohs@gmail.com>
2018-11-09 15:58:59 +0100

    Created controller routes for new, pending and resume Added menu navigation

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -9,13 +9,39 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @IsGranted("ROLE_USER")
+ */
 class PlayerController extends AbstractController
 {
     /**
-     * @Route("/games", name="app_played_games")
-     * @IsGranted("ROLE_USER")
+     * @Route("/new", name="app_new_game")
      */
-    public function playedGames(CodebreakerRepository $codebreakers, Request $request): Response
+    public function start(Request $request): Response
+    {
+        return new Response('new game');
+    }
+
+    /**
+     * @Route("/games", name="app_pending_games")
+     */
+    public function pending(Request $request): Response
+    {
+        return new Response('resume');
+    }
+
+    /**
+     * @Route("/resume/{id}", name="app_resume_game")
+     */
+    public function resume(Request $request): Response
+    {
+        return new Response('resume_one');
+    }
+
+    /**
+     * @Route("/played", name="app_played_games")
+     */
+    public function played(CodebreakerRepository $codebreakers, Request $request): Response
     {
         $games = $codebreakers->finishedGames(
             $this->getUser(),
